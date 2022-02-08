208f6610db50f22040ff6304b37195c05792fd50
AniKal25 <kal21006@byui.edu>
2022-02-08 01:08:22 +0100

    Player code

---
+++ player.py
@@ -0,0 +1,30 @@
+#from game import word
+
+# Create a class for this 
+class Player:
+
+    def __init__(self):
+
+        self._theme = ""
+        self._guess = ""
+
+
+    def choose_theme(self, theme):
+
+        """
+        This will get the theme from the player
+        """
+        self._theme = pick(theme)
+
+    def guess(self, guess):
+        """
+        This will allow the player to guess the word
+        """
+        self._guess = guess
+
+    def get_guess(self):
+        """
+        This will return the current guess
+        """
+        guess = self._guess
+        return guess
